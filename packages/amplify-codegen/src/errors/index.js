db4a8a6f5e00fc6353bea10685a520a66e6f7884
Christopher Sundersingh <sundersc@amazon.com>
2023-05-04 13:50:13 -0700

    feat: major version bump
    BREAKING CHANGE: major version bump

--- packages/amplify-codegen/src/errors/index.js
+++ packages/amplify-codegen/src/errors/index.js
@@ -5,7 +5,6 @@ class AmplifyCodeGenNoProjectAvailableError extends Error {}
 
 class AmplifyCodeGenAPINotFoundError extends Error {}
 class AmplifyCodeGenAPIPendingPush extends Error {}
-
 module.exports = {
   AmplifyCodeGenNotSupportedError,
   AmplifyCodeGenNoAppSyncAPIAvailableError,
